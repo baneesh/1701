ab397e9150d67c3076db0b579ff531000f6a32d4
deepakchaula <yess_bose@yahoo.com>
2014-04-23 11:47:10 +0530

    Women and Bespoke slides panels

---
+++ js/custom-bxslider.js
@@ -0,0 +1,24 @@
+  $('.slider1').bxSlider({
+    slideWidth: 186,
+    minSlides: 1,
+    maxSlides: 7,
+    slideMargin: 0
+  });
+  $('.slider2').bxSlider({
+    slideWidth: 396,
+    minSlides: 1,
+    maxSlides: 7,
+    slideMargin: 0
+  });
+  $('.slider3').bxSlider({
+    slideWidth: 186,
+    minSlides: 1,
+    maxSlides: 7,
+    slideMargin: 0
+  });
+  $('.slider4').bxSlider({
+    slideWidth: 186,
+    minSlides: 1,
+    maxSlides: 7,
+    slideMargin: 0
+  });
