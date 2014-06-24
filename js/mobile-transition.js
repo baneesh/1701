5ebacecacd4a9bfc7a5b4b4715bdb5650d427711
deepakchaula <yess_bose@yahoo.com>
2014-06-24 13:51:37 +0530

    more product pages

--- js/mobile-transition.js
+++ js/mobile-transition.js
@@ -29,5 +29,26 @@ $(document).ready(function() {
 		return false;
 		
 	});
+	
+	$('.header-elements a').click(function(){
+								  
+		var toLoad = $(this).attr('href')+' .container';
+		$('.container').hide('fast',loadContent);
+		$('#load').remove();
+		$('.mainSectionMobile').append('<span id="load">LOADING...</span>');
+		$('#load').fadeIn('normal');
+		//window.location.hash = $(this).attr('href').substr(0,$(this).attr('href').length-5);
+		function loadContent() {
+			$('.container').load(toLoad,'',showNewContent())
+		}
+		function showNewContent() {
+			$('.container').show('normal',hideLoader());
+		}
+		function hideLoader() {
+			$('#load').fadeOut('normal');
+		}
+		return false;
+		
+	});
 
 });
